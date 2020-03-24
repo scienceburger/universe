b38d705370db6f97944cc8a48de52769f316a104
scienceburger <karageuzian.jb@gmail.com>
2020-03-24 21:07:43 +0000

    wire framing initial routes

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+order_queue = []
 
 @app.route('/')
 def hello_whale():
     return "Whale, Hello there!"
+
+
+@app.route('/add_to_orders')
+def add_to_order_queue():
+    order_queue.append('something')
+    return 'thing done'
+
+@app.route('/list_orders')
+def list_orders():
+    return "\n".join(order_queue)
+
+
+@app.route('/update')
+def update():
+    return 'Done!'
 
 
 if __name__ == '__main__':
