e7d2652122ba6554fab310f3fd8c5ad6607e6ffd
scienceburger <karageuzian.jb@gmail.com>
2020-03-28 12:23:36 +0000

    a few stuff there. (basic templating, auth that actually checks something and other stuff )

--- player.py
+++ player.py
@@ -1,7 +1,10 @@
 class Player:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, username: str):
+        self.username = username
+        self.display_name = username
+        self.planets = []
+
 
     def update(self):
         return 0
---
+++ templates.py
@@ -0,0 +1,3 @@
+UNIVERSE_TITLE = "U•N•I•V•E•R•S•E"
+SIGNUP_BUTTON = '''<input type='submit' name='signup' value='Sign me up!'>'''
+LOGIN_BUTTON = '''<input type='submit' name='login' value='Log me in!'>'''
